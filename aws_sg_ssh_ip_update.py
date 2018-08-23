0c35d431d8ac8e5e87e23c42d625d7c0208e1abe
David A J Simpson <dajsimpson@gmail.com>
2018-08-23 17:44:32 +0100

    Added option to use a PORT number, rather than just hard coded port 22 SSH

--- aws_sg_ssh_ip_update.py
+++ aws_sg_ssh_ip_update.py
@@ -21,20 +21,20 @@ import datetime, requests, boto3
 #             NEW_IP - The IP address that is required for the SSH rule
 #
 # =============================================================================
-def updateSecurityGroup(SG_ID,NEW_IP):
+def updateSecurityGroup(SG_ID,NEW_IP,PORT):
 
    # Get the Security Group information
    ec2 = boto3.resource('ec2')
    try:
       sg = ec2.SecurityGroup(SG_ID)
       for ipp in sg.ip_permissions:
-         if 22 == ipp['FromPort']:
+         if PORT == ipp['FromPort']:
             ipR = ipp['IpRanges']
    except:
       print("ERROR: Problem getting information about Security Group with ID: "+SG_ID)
       return
 
-   # Check that an existing SSH (22) rule was found
+   # Check that an existing rule was found
    try:
       ipR
    except NameError:
@@ -51,22 +51,22 @@ def updateSecurityGroup(SG_ID,NEW_IP):
    # If the IPs are not the same then some updates are required
    if NEW_IP != SG_IP :
       if 1 == Do_Revoke :
-         # Revoke the old port SSH (22) rule
+         # Revoke the old port rule
          try:
-            sg.revoke_ingress( DryRun=False, IpPermissions=[{'FromPort':22,'IpProtocol':'tcp','IpRanges':[{'CidrIp':SG_IP},],'Ipv6Ranges':[],'PrefixListIds':[],'ToPort':22,'UserIdGroupPairs':[]}] )
+            sg.revoke_ingress( DryRun=False, IpPermissions=[{'FromPort':PORT,'IpProtocol':'tcp','IpRanges':[{'CidrIp':SG_IP},],'Ipv6Ranges':[],'PrefixListIds':[],'ToPort':PORT,'UserIdGroupPairs':[]}] )
          except:
-            print("ERROR: Removal of the incorrect SSH rule has failed for Security Group: "+SG_ID)
+            print("ERROR: Removal of the incorrect rule has failed for Security Group: "+SG_ID)
             print("Please login to the AWS Management Console and check")
             return
             
       try:
-      	sg.authorize_ingress( DryRun=False, IpPermissions=[{'FromPort':22,'IpProtocol':'tcp','IpRanges':[{'CidrIp':NEW_IP,'Description':DT_INFO},],'Ipv6Ranges':[],'PrefixListIds':[],'ToPort':22,'UserIdGroupPairs':[]}] )
+      	sg.authorize_ingress( DryRun=False, IpPermissions=[{'FromPort':PORT,'IpProtocol':'tcp','IpRanges':[{'CidrIp':NEW_IP,'Description':DT_INFO},],'Ipv6Ranges':[],'PrefixListIds':[],'ToPort':PORT,'UserIdGroupPairs':[]}] )
       except:
-         print("ERROR: Adding the new SSH rule has failed for Security Group: "+SG_ID)
+         print("ERROR: Adding the new rule has failed for Security Group: "+SG_ID)
          print("Please login to the AWS Management Console and check")
          return
 
-      print("Update made to the Security Group ("+SG_ID+") to enable SSH (22) access from IP: "+NEW_IP)
+      print("Update made to the Security Group ("+SG_ID+") to enable port PORT access from IP: "+NEW_IP)
    else:
       print("No updates were required to Security Group: "+SG_ID)
 
@@ -90,8 +90,12 @@ My_IP = ip_request.text
 My_IP=''.join([My_IP.rstrip(),"/32"])
 
 
-updateSecurityGroup('sg-38058e50',My_IP)
-updateSecurityGroup('sg-0a9d3cd5337b69a45',My_IP)
+updateSecurityGroup('sg-38058e50',My_IP,22)
+updateSecurityGroup('sg-38058e50',My_IP,80)
+updateSecurityGroup('sg-38058e50',My_IP,443)
+updateSecurityGroup('sg-0a9d3cd5337b69a45',My_IP,22)
+updateSecurityGroup('sg-0a9d3cd5337b69a45',My_IP,80)
+updateSecurityGroup('sg-0a9d3cd5337b69a45',My_IP,443)
 
 #                                  Done
 # =============================================================================
